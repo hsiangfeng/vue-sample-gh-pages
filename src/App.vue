a94ef866d78ff171e9deb65a72db75b2ca41ed3e
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-09-07 10:14:27 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ElButton type="primary">這是一個按鈕</ElButton>
+  <el-button type="primary">這是一個按鈕</el-button>
 </template>
 
 <script>
@@ -10,6 +12,13 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    console.log('process.env.VUE_APP_APIURL', process.env.VUE_APP_APIURL)
+
+    this.$http.get(process.env.VUE_APP_APIURL).then((response) => {
+      console.log(response.data)
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+
+app.use(ElementPlus)
+
+app.mount('#app')
